f5811abf0faa6e684c3e17b497a331a63e7b00e2
danieliraz <dan.eliraz@gmail.com>
2018-07-05 16:21:36 +0300

    fix bug

--- src/server/api.js
+++ src/server/api.js
@@ -252,7 +252,7 @@ const Api = {
         {'Content-Type': contetType}
       )
     });
-    filename = filename.replace(/[^a-zA-Z0-9@\.\ ~_-]+/g, '');
+    fileName = fileName.replace(/[^a-zA-Z0-9@\.\ ~_-]+/g, '');
     return _filesApi.post(`${filesPath}${fileName}`, file);
   }
 };
